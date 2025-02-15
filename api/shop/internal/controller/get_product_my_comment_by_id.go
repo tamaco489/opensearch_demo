b58147c9b8b9cb745f84cf296724b25b2b27df0a
tamaco489 <tamaco489@gmail.com>
2025-02-16 02:16:17 +0900

    add get product my comment id api logic

--- api/shop/internal/controller/get_product_my_comment_by_id.go
+++ api/shop/internal/controller/get_product_my_comment_by_id.go
@@ -1,11 +1,26 @@
 package controller
 
 import (
+	"time"
+
 	"github.com/gin-gonic/gin"
 	"github.com/tamaco489/elasticsearch_demo/api/shop/internal/gen"
 )
 
 func (c *Controllers) GetProductMyCommentByID(ctx *gin.Context, request gen.GetProductMyCommentByIDRequestObject) (gen.GetProductMyCommentByIDResponseObject, error) {
 
-	return gen.GetProductMyCommentByID200JSONResponse{}, nil
+	jst := time.FixedZone("JST", 9*60*60)
+
+	return gen.GetProductMyCommentByID200JSONResponse{
+		Id:        54009221,
+		Title:     "とても良い商品です",
+		Content:   "この商品は非常に良いです。特にデザインが素晴らしい。",
+		CreatedAt: time.Date(2025, 2, 15, 13, 45, 30, 0, jst),
+		LikeCount: 15,
+		Rate:      4,
+		ReportReasons: []gen.ReportReason{
+			gen.Inappropriate,
+			gen.Irrelevant,
+		},
+	}, nil
 }
