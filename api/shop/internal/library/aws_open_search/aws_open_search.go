fb9725f891315c35744dae537f422e2ddb0284cd
tamaco489 <tamaco489@gmail.com>
2025-02-16 18:19:43 +0900

    add new open search api client

---
+++ api/shop/internal/library/aws_open_search/aws_open_search.go
@@ -0,0 +1,80 @@
+package open_search
+
+import (
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/opensearch-project/opensearch-go/v4"
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
+	"github.com/tamaco489/elasticsearch_demo/api/shop/internal/configuration"
+
+	requestsigner "github.com/opensearch-project/opensearch-go/v4/signer/awsv2"
+)
+
+// NewOpenSearchAPIClient:
+//
+// NOTE: https://opensearch.org/docs/latest/clients/go/
+func NewOpenSearchAPIClient(awsCfg aws.Config) (*opensearchapi.Client, error) {
+
+	var endpoint = ""
+
+	switch configuration.Get().API.Env {
+	case "dev":
+		endpoint = "http://opensearch:9200"
+
+	case "stg":
+		//
+
+	default:
+		//
+	}
+
+	// Create an opensearch client and use the request-signer.
+	client, err := opensearchapi.NewClient(
+		opensearchapi.Config{
+			Client: opensearch.Config{
+				Addresses: []string{endpoint},
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
+
+// NewOpenSearchAPIClientWithSigner:
+func NewOpenSearchAPIClientWithSigner(awsCfg aws.Config) (*opensearchapi.Client, error) {
+
+	var endpoint = ""
+
+	switch configuration.Get().API.Env {
+	case "dev":
+		endpoint = "https://localhost:9200"
+
+	case "stg":
+		//
+
+	default:
+		//
+	}
+
+	// Create an AWS request Signer and load AWS configuration using default config folder or env vars.
+	signer, err := requestsigner.NewSignerWithService(awsCfg, "es") // Use "aoss" for Amazon OpenSearch Serverless
+	if err != nil {
+		return nil, err
+	}
+	// Create an opensearch client and use the request-signer.
+	client, err := opensearchapi.NewClient(
+		opensearchapi.Config{
+			Client: opensearch.Config{
+				Addresses: []string{endpoint},
+				Signer:    signer,
+			},
+		},
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	return client, nil
+}
--- api/shop/internal/usecase/create_product_comment.go
+++ api/shop/internal/usecase/create_product_comment.go
@@ -2,8 +2,11 @@ package usecase
 
 import (
 	"context"
+	"log"
 
+	"github.com/tamaco489/elasticsearch_demo/api/shop/internal/configuration"
 	"github.com/tamaco489/elasticsearch_demo/api/shop/internal/gen"
+	open_search "github.com/tamaco489/elasticsearch_demo/api/shop/internal/library/aws_open_search"
 )
 
 type CreateProductCommentUseCase struct {
@@ -16,6 +19,19 @@ func NewCreateProductComment() *CreateProductCommentUseCase {
 
 // CreateProductComment は商品に対してコメントを投稿します。
 func (u *CreateProductCommentUseCase) CreateProductComment(ctx context.Context, request gen.CreateProductCommentRequestObject) (gen.CreateProductCommentResponseObject, error) {
+
+	awsCfg := configuration.Get().AWSConfig
+	client, err := open_search.NewOpenSearchAPIClient(awsCfg)
+	if err != nil {
+		return nil, err
+	}
+
+	res, err := client.Ping(ctx, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	log.Println("[info] result ping to open search", res)
 
 	return gen.CreateProductComment201JSONResponse{
 		Id: 70235591,
