ecc968de4f6ea5e683412b5f4ddb665cb213dabb
tamaco489 <tamaco489@gmail.com>
2025-02-18 00:11:33 +0900

    fix product comment entity

--- api/shop/internal/domain/dta/product_comment.go
+++ api/shop/internal/domain/dta/product_comment.go
@@ -9,8 +9,8 @@ import (
 // DTAはエンティティとDTO間でのデータ変換を担当します。
 // 例えば、外部システム（API）から受け取ったデータをエンティティに変換したり、エンティティを外部システムに適した形に変換したりします。
 
-// ToEntity は OpenAPI のリクエストDTOをエンティティに変換します。
-func ToEntity(request gen.CreateProductCommentRequestObject, commentID, userID uint64) *entity.ProductComment {
+// ToProductCommentEntity は OpenAPI のリクエストDTOをエンティティに変換します。
+func ToProductCommentEntity(request gen.CreateProductCommentRequestObject, commentID, userID uint64) *entity.ProductComment {
 	return entity.NewProductComment(
 		commentID,
 		request.ProductID,
