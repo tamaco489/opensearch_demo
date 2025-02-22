3dba186d06d143b4871e5c20f1862375960cc49b
Masato Takeuchi <tamaco489@gmail.com>
2025-02-23 04:17:21 +0900

    fix admin product comment violations api di (#18)
    * fix api spec description
    
    * fix README.md
    
    * fix di

---
+++ api/shop/internal/usecase/get_product_comment_violations.go
@@ -0,0 +1,77 @@
+package usecase
+
+import (
+	"context"
+	"time"
+
+	"github.com/tamaco489/elasticsearch_demo/api/shop/internal/gen"
+)
+
+// GetProductCommentViolations: 商品に対して投稿されたコメントの中で、予め定めたNGワードに該当するデータを取得します。
+func (u productCommentUseCase) GetProductCommentViolations(ctx context.Context, request gen.GetProductCommentViolationsRequestObject) (gen.GetProductCommentViolationsResponseObject, error) {
+
+	// NOTE: 大量の商品データ、及びコメントデータを取得する想定とし、1500ms(1.5秒) 遅延させる。
+	time.Sleep(1500 * time.Millisecond)
+
+	jst := time.FixedZone("JST", 9*60*60)
+
+	ngComments := []gen.GetProductCommentViolations{
+		{
+			Id:        54009221,
+			Title:     "とても良い商品です",
+			Content:   "この商品は非常に良いです。特にデザインが素晴らしい。",
+			CreatedAt: time.Date(2025, 2, 15, 13, 45, 30, 0, jst),
+			LikeCount: 15,
+			ReportReasons: []gen.ReportReason{
+				gen.Inappropriate,
+				gen.Irrelevant,
+			},
+			User: gen.CommentByUser{
+				UserId:    12345,
+				UserName:  "氷織 羊",
+				AvatarUrl: "https://example.com/avatar.jpg",
+			},
+		},
+		{
+			Id:        54009226,
+			Title:     "デザインが気に入った",
+			Content:   "商品は思った通りのデザインでとても気に入りました。",
+			CreatedAt: time.Date(2025, 2, 16, 14, 25, 30, 0, jst),
+			LikeCount: 10,
+			ReportReasons: []gen.ReportReason{
+				gen.Fake,
+			},
+			User: gen.CommentByUser{
+				UserId:    23456,
+				UserName:  "御影 玲王",
+				AvatarUrl: "https://example.com/avatar2.jpg",
+			},
+		},
+		{
+			Id:        54009533,
+			Title:     "ちょっと期待外れ",
+			Content:   "使用感は良かったが、価格に見合わないかもしれない。",
+			CreatedAt: time.Date(2025, 2, 17, 15, 15, 30, 0, jst),
+			LikeCount: 5,
+			ReportReasons: []gen.ReportReason{
+				gen.Other,
+			},
+			User: gen.CommentByUser{
+				UserId:    34567,
+				UserName:  "乙夜 影汰",
+				AvatarUrl: "https://example.com/avatar3.jpg",
+			},
+		},
+	}
+
+	nextCursor := gen.GetProductCommentViolationsNextCursor{
+		NextCursor: "NTQwMDk1MzY=",
+	}
+
+	response := gen.GetProductCommentViolations200JSONResponse{
+		NgComments: ngComments,
+		Metadata:   nextCursor,
+	}
+
+	return response, nil
+}
--- api/shop/internal/usecase/usecase_interface.go
+++ api/shop/internal/usecase/usecase_interface.go
@@ -3,9 +3,31 @@ package usecase
 import (
 	"context"
 
+	"github.com/opensearch-project/opensearch-go/v4/opensearchapi"
 	"github.com/tamaco489/elasticsearch_demo/api/shop/internal/gen"
 )
 
+// IProductCommentUseCase は、商品コメントに関するユースケースを提供するインターフェースです。
 type IProductCommentUseCase interface {
+	// ユーザ向けAPI: CreateProductComment（商品に対して任意のコメントを投稿します。）
 	CreateProductComment(ctx context.Context, request gen.CreateProductCommentRequestObject) (gen.CreateProductCommentResponseObject, error)
+
+	// 管理者向けAPI: GetProductCommentViolations（商品に対して投稿されたコメントの中で、予め定めたNGワードに該当するデータを取得します）
+	GetProductCommentViolations(ctx context.Context, request gen.GetProductCommentViolationsRequestObject) (gen.GetProductCommentViolationsResponseObject, error)
+}
+
+// productCommentUseCase は、商品コメントに関連するユースケースを処理する具体的な実装です。
+// IProductCommentUseCase インターフェースを満たし、OpenSearch を使用して商品コメントの作成や
+// NG ワードを含むコメントの取得などの機能を提供します。
+type productCommentUseCase struct {
+	// OpenSearch API クライアントを使用してデータの検索・登録を行います。
+	opsApiClient *opensearchapi.Client
+}
+
+// NewCreateProductComment は、productCommentUseCase のコンストラクタ関数です。
+// 新しい productCommentUseCase インスタンスを生成し、IProductCommentUseCase を実装したものを返します。
+func NewCreateProductComment(opsApiClient *opensearchapi.Client) IProductCommentUseCase {
+	return &productCommentUseCase{
+		opsApiClient: opsApiClient,
+	}
 }
