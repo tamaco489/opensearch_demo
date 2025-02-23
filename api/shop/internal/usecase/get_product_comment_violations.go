2047de894d3b2010d430bfe211ef559461528397
tamaco489 <tamaco489@gmail.com>
2025-02-23 23:13:43 +0900

    fix usecase

--- api/shop/internal/usecase/get_product_comment_violations.go
+++ api/shop/internal/usecase/get_product_comment_violations.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 	"time"
 
@@ -34,25 +35,27 @@ func (u productCommentUseCase) GetProductCommentViolations(ctx context.Context, 
 		return nil, fmt.Errorf("failed to search comments with NG words: %v", err)
 	}
 
-	ngComments := make([]gen.GetProductCommentViolations, 0, len(searchResult.Hits.Hits))
-	for _, hit := range searchResult.Hits.Hits {
-		var comment gen.GetProductCommentViolations
+	ngComments := make([]gen.GetProductCommentViolations, len(searchResult.Hits.Hits))
+	for i, hit := range searchResult.Hits.Hits {
 
 		// 中間構造体でまずパースする
 		var intermediate struct {
+			Id        uint64 `json:"id"`
+			Title     string `json:"title"`
 			Content   string `json:"content"`
 			CreatedAt string `json:"created_at"`
-			Id        uint64 `json:"id"`
 			ProductId uint64 `json:"product_id"`
 			Rate      uint32 `json:"rate"`
-			// ReportReasons []ReportReason `json:"report_reasons"`
-			Title  string `json:"title"`
-			UserID uint64 `json:"user_id"`
-			// User          CommentByUser  `json:"user"`
+			UserID    uint64 `json:"user_id"`
 		}
 
 		if err := json.Unmarshal(hit.Source, &intermediate); err != nil {
-			return nil, fmt.Errorf("failed to unmarshal comment: %v", err)
+			return gen.GetProductCommentViolations500Response{}, fmt.Errorf("failed to unmarshal comment: %v", err)
+		}
+
+		commentID, err := strconv.ParseUint(hit.ID, 10, 64)
+		if err != nil {
+			return gen.GetProductCommentViolations500Response{}, fmt.Errorf("failed to parse product comment ID: %v", err)
 		}
 
 		// 日付文字列を time.Time に変換
@@ -60,26 +63,24 @@ func (u productCommentUseCase) GetProductCommentViolations(ctx context.Context, 
 		if err != nil {
 			parsedTime, err = time.Parse(time.RFC3339, intermediate.CreatedAt)
 			if err != nil {
-				return nil, fmt.Errorf("failed to parse CreatedAt: %v", err)
+				return gen.GetProductCommentViolations500Response{}, fmt.Errorf("failed to parse CreatedAt: %v", err)
 			}
 		}
 
-		// intermediate から最終的な構造体に値をセット
-		comment.Content = intermediate.Content
-		comment.CreatedAt = parsedTime
-		comment.Id = intermediate.Id
-		comment.ProductId = intermediate.ProductId
-		comment.Rate = intermediate.Rate
-		comment.ReportReasons = []gen.ReportReason{} // NOTE: 別途RDS等で管理しているものをuidで取得する
-		comment.Title = intermediate.Title
-
-		// user構造体
-		comment.User.UserId = intermediate.UserID
-		comment.User.UserName = ""                                                                           // NOTE: 別途RDS等で管理しているものをuidで取得する
-		comment.User.AvatarUrl = fmt.Sprintf("https://example.com/users/%d/avatar.jpg", intermediate.UserID) // NOTE: 別途RDS等で管理しているものをuidで取得する
-
-		// NGコメントを追加
-		ngComments = append(ngComments, comment)
+		ngComments[i] = gen.GetProductCommentViolations{
+			Id:            commentID,
+			Content:       intermediate.Content,
+			Title:         intermediate.Title,
+			CreatedAt:     parsedTime,
+			ProductId:     intermediate.ProductId,
+			ReportReasons: []gen.ReportReason{}, // NOTE: 別途RDS等で管理しているものをuidで取得する
+			Rate:          intermediate.Rate,
+			User: gen.CommentByUser{
+				UserId:    intermediate.UserID,
+				UserName:  "",                                                                          // NOTE: 別途RDS等で管理しているものをuidで取得する
+				AvatarUrl: fmt.Sprintf("https://example.com/users/%d/avatar.jpg", intermediate.UserID), // NOTE: 別途RDS等で管理しているものをuidで取得する
+			},
+		}
 	}
 
 	// NOTE: cursorの値は一旦固定値とする
