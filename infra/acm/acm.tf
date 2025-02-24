2120029305765b4b547865347b03eb91402649e1
tamaco489 <tamaco489@gmail.com>
2025-02-24 23:46:29 +0900

    fix acm variables

--- infra/acm/acm.tf
+++ infra/acm/acm.tf
@@ -9,13 +9,13 @@ resource "aws_acm_certificate" "opensearch_demo" {
 
   tags = {
     Env     = var.env
-    Project = var.product
-    Name  = "${local.fqn}-api"
+    Project = var.project
+    Name    = "${var.env}-${var.project}" // api v1, v2... etc となる想定のため
   }
 }
 
 # API Gateway カスタムドメイン
-resource "aws_apigatewayv2_domain_name" "opensearch_demo_apiv1" {
+resource "aws_apigatewayv2_domain_name" "shop_api_v1" {
   domain_name = "apiv1.${data.terraform_remote_state.route53.outputs.host_zone.name}"
 
   domain_name_configuration {
@@ -26,14 +26,14 @@ resource "aws_apigatewayv2_domain_name" "opensearch_demo_apiv1" {
 }
 
 # Route53 Aレコード
-resource "aws_route53_record" "opensearch_demo_apiv1" {
+resource "aws_route53_record" "shop_api_v1" {
   zone_id = data.terraform_remote_state.route53.outputs.host_zone.id
   name    = "apiv1.${data.terraform_remote_state.route53.outputs.host_zone.name}"
   type    = "A"
 
   alias {
-    name                   = aws_apigatewayv2_domain_name.opensearch_demo_apiv1.domain_name_configuration.0.target_domain_name
-    zone_id                = aws_apigatewayv2_domain_name.opensearch_demo_apiv1.domain_name_configuration.0.hosted_zone_id
+    name                   = aws_apigatewayv2_domain_name.shop_api_v1.domain_name_configuration.0.target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.shop_api_v1.domain_name_configuration.0.hosted_zone_id
     evaluate_target_health = false
   }
 }
