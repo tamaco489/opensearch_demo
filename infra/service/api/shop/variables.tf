a473444c179cca72f308604e7a7ec3d87b96cde6
tamaco489 <tamaco489@gmail.com>
2025-02-25 01:05:52 +0900

    add shop api for lambda service

---
+++ infra/service/api/shop/variables.tf
@@ -0,0 +1,21 @@
+variable "env" {
+  description = "The environment in which the shop api lambda will be created"
+  type        = string
+  default     = "dev"
+}
+
+variable "project" {
+  description = "The project name"
+  type        = string
+  default     = "opensearch-demo"
+}
+
+variable "product" {
+  description = "The product name"
+  type        = string
+  default     = "shop"
+}
+
+locals {
+  fqn = "${var.env}-${var.product}"
+}
