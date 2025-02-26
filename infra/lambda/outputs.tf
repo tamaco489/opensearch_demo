023b8a624e5d7a0e25545924847042b8700ca280
tamaco489 <tamaco489@gmail.com>
2025-02-27 00:22:50 +0900

    destroy lambda logging iam role resource

--- infra/lambda/outputs.tf
+++
@@ -1,7 +0,0 @@
-output "iam" {
-  value = {
-    lambda_logging_policy_arn  = aws_iam_policy.lambda_logging.arn
-    lambda_logging_policy_id   = aws_iam_policy.lambda_logging.id
-    lambda_logging_policy_name = aws_iam_policy.lambda_logging.name
-  }
-}
