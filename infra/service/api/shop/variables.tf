c0bb47c4f4b76cd1cef644de05f21a61ef7b5061
tamaco489 <tamaco489@gmail.com>
2025-02-27 03:36:47 +0900

    fix shop api lambda open search iam role

--- infra/service/api/shop/variables.tf
+++ infra/service/api/shop/variables.tf
@@ -16,6 +16,19 @@ variable "product" {
   default     = "shop"
 }
 
+variable "region" {
+  description = "The region in which the shop api lambda will be created"
+  type        = string
+  default     = "ap-northeast-1"
+}
+
+variable "aws_account_id" {
+  description = "The aws account id"
+  type        = string
+  default     = "1234567890"
+}
+
 locals {
-  fqn = "${var.env}-${var.product}"
+  fqn             = "${var.env}-${var.product}"
+  collection_name = "${var.env}-${var.project}-collection"
 }
