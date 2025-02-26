f6686c78c9ac28cf523b186f8f64fbca9c79e87a
tamaco489 <tamaco489@gmail.com>
2025-02-27 00:17:26 +0900

    add egress security group

---
+++ infra/service/api/shop/security_group.tf
@@ -0,0 +1,18 @@
+resource "aws_security_group" "shop_api" {
+  name        = "${local.fqn}-api"
+  description = "Security group for API service running in the VPC"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc.id
+
+  tags = {
+    Env     = var.env
+    Project = var.project
+    Name    = "${local.fqn}-api"
+  }
+}
+
+resource "aws_vpc_security_group_egress_rule" "shop_api_egress" {
+  security_group_id = aws_security_group.shop_api.id
+  description       = "Allow Lambda to access external resources (e.g., RDS, OpenSearch, APIs)"
+  ip_protocol       = "-1"
+  cidr_ipv4         = "0.0.0.0/0"
+}
